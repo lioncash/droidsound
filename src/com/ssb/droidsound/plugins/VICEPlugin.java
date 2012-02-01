8d3dc3f29d612961a29c26ece4797a2a74660072
lioncash <mathew1900@hotmail.com>
2012-01-31 20:54:57 -0500

    Update VICE to 2.3.14 (revision 25204)

--- src/com/ssb/droidsound/plugins/VICEPlugin.java
+++ src/com/ssb/droidsound/plugins/VICEPlugin.java
@@ -359,6 +359,6 @@ public class VICEPlugin extends DroidSoundPlugin {
 
 	@Override
 	public String getVersion() {
-		return "VICEPlugin (vsid r25200)";
+		return "VICEPlugin (vsid r25204))";
 	}
 }
