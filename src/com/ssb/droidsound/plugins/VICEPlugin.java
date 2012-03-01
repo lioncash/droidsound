2ee719466338f559e002c536ba3561af7c6bcd95
lioncash <mathew1900@hotmail.com>
2012-03-01 13:35:41 -0500

    Updated VICE to revision 25357

--- src/com/ssb/droidsound/plugins/VICEPlugin.java
+++ src/com/ssb/droidsound/plugins/VICEPlugin.java
@@ -359,6 +359,6 @@ public class VICEPlugin extends DroidSoundPlugin {
 
 	@Override
 	public String getVersion() {
-		return "VICEPlugin (vsid r25356))";
+		return "VICEPlugin (vsid r25357))";
 	}
 }
